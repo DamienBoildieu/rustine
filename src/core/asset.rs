b80a46619067acd5d63e2ba1ec7d89ec53756182
Damien Boildieu <damien.boildieu@laposte.net>
2025-10-17 18:46:36 -0400

    add id to asset

--- src/core/asset.rs
+++ src/core/asset.rs
@@ -1,28 +1,39 @@
-use std::rc::{Rc, Weak};
+use std::{collections::HashMap};
 
 pub enum Handle<T> {
-    Strong(Rc<T>),
-    Weak(Weak<T>),
+    Strong(Asset<T>),
+    Weak(Asset<T>),
 }
 
 pub struct Asset<T> {
+    pub id: u64,
     pub data: T,
 }
 
 pub struct AssetManager<T> {
-    // Asset storage and management fields would go here
-    pub assets: Vec<Asset<T>>,
+    assets: HashMap<u64, Asset<T>>,
+    next_id: u64,
 }
 
-impl<T> AssetManager<T> {
-    pub fn add_asset(&mut self, data: Asset<T>) -> &Asset<T> {
-        self.assets.push(data);
-        self.assets.last().unwrap()
+impl<T> Drop for Handle<T> {
+    fn drop(&mut self) {
+        todo!()
     }
 }
 
-impl<T> From<T> for Asset<T> {
-    fn from(value: T) -> Self {
-        Self { data: value }
+impl<T> AssetManager<T> {
+    pub fn create_asset(&mut self, data: T) -> &Asset<T> {
+        let id = self.next_id;
+        self.next_id += 1;
+        let asset = Asset {
+            id,
+            data
+        };
+        self.assets.insert(id, asset);
+        self.assets.get(&id).unwrap()
+    }
+
+    pub fn get_asset(&self, id: u64) -> Option<&Asset<T>> {
+        self.assets.get(&id)
     }
 }
